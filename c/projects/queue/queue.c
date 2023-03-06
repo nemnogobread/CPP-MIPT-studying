613889c0c7e66742a5da9448567f584cb4cc9261
nemnogobread <glebmiloslavov@gmail.com>
2023-03-06 16:52:18 +0300

    add pointers on queue head and queue tail. Debug version

--- c/projects/queue/queue.c
+++ c/projects/queue/queue.c
@@ -9,42 +9,64 @@ typedef struct Node
 }Node;
 Node *queue = NULL;
 
+typedef struct Queue
+{
+    struct Node *head;
+    struct Node *tail;
+}Queue;
+
 
 void print_queue(Node *queue);
-void push_in_queue(Node **queue, int data);
+void enqueue(Node **queue, int data);
+int dequeue(Node *queue);
 
 
 int main()
 {
     int test[] = {3, 17, 21, 10};
-    Node *queue = NULL;
+    Queue queue = {NULL, NULL}; 
 
     for (int i = 0; i < sizeof(test)/sizeof(test[0]); i++)
     {
-        push_in_queue(&queue, test[i]);
+        enqueue(&queue.head, test[i]);
+        if (i==0)
+            queue.tail = queue.head;
+        
+        printf("date from first: %d \n", *queue.head);
+        printf("date from tail: %d \n", *queue.tail);
+        printf("\n");
     }
 
-    print_queue(queue);
+    print_queue(queue.head);
+
+    printf("date from first: %d \n", *queue.head);
+    printf("date from tail: %d \n", *queue.tail);
+
     return 0;
 }
 
 
-void print_queue(Node *queue)
+void print_queue(Node *queue_head)
 {
-    if (queue == NULL)
+    if (queue_head == NULL)
         printf("No elements in queue");
 
-    for ( Node *p = queue ; p != NULL ; p = p->next)
+    for (Node *p = queue_head; p != NULL; p = p->next)
     {
         printf("%d ", p->data);
     }
     printf("\n");
 }
 
-void push_in_queue(Node **queue, int data)
+void enqueue(Node **queue_head, int data)
 {
     Node *p = (Node*)malloc(sizeof(Node));
     p->data = data;
-    p->next = *queue;
-    *queue = p;
+    p->next = *queue_head;
+    *queue_head = p;
+}
+
+int dequeue(Node *queue)
+{
+    return 0;
 }
