47cb83403aa3c8d321f28c76229bdcb2d8b87163
nemnogobread <glebmiloslavov@gmail.com>
2023-03-08 18:20:52 +0300

    add general comments

--- c/projects/queue/queue on cycled list.c
+++ c/projects/queue/queue on cycled list.c
@@ -1,34 +1,37 @@
+// в ходе работы программы предпологается, что работа идёт только с неотрицательными числами
+// в противном случае, нужно подправить main и функцию dequeue 
+
 #include "stdio.h"
 #include "stdlib.h"
 #include "inttypes.h"
 
 typedef struct Queue
 {
-    int *data;
-    int head;
-    int tail;
-    int count;
-    int max;
+    int *data;  // массив, который мы замкнём в процессе реализации
+    int head;   // номер первого элемента в очереди
+    int tail;   // номер последнего элемента в очереди
+    int count;  // кол-во элементов в очереди
+    int max;    // максимальный размер очереди
 }Queue;
 
 Queue *init(size_t size);
-void enqueue(Queue *q, int a);
-int dequeue(Queue *q);
-void print_queue(Queue *q);
-void print_data(Queue *q);
+void enqueue(Queue *q, int a);                          // добавление в конец очереди
+int dequeue(Queue *q);                                  // добавление в конец очереди
+void print_queue(Queue *q);                             // печать очереди
+void print_data(Queue *q);                              // вспомогательная функция, печать всего массива data        
 
 int main()
 {
     int test[] = {1, 2, 3, 4, 5, 6};  
     uint8_t size;
     printf("Please, enter max size of queue: ");
-    scanf("%d", &size);
+    scanf("%d", &size);                                 // чтобы убедиться в зацикленности массива, набери size = 9
     Queue *q = init(size);
 
-    for (int i = 0; i < 12; i++)
+    for (int i = 0; i < 12; i++)                        // советую просто поставить в цикле точку останова и пройтись дебагером, чтобы всё понять
     {
-        enqueue(q, test[i%6]);
-        if (i%2)
+        enqueue(q, test[i%6]);                         
+        if (i%2)                                        
             dequeue(q);
         puts("*data has next structure: ");
         print_data(q);
@@ -49,10 +52,10 @@ int main()
     }
 }
 
-Queue *init(size_t size)
+Queue *init(size_t size)                                // инициализируем очередь
 {
-    Queue *q = (Queue*)malloc(sizeof(Queue));
-    q->data = (int*)calloc(size, sizeof(int));
+    Queue *q = (Queue*)malloc(sizeof(Queue));           // выделение памяти под очередь
+    q->data = (int*)calloc(size, sizeof(int));          // выделение памяти под массив data. изначально он заполнится нулями
     q->head = q->tail = q->count = 0;
     q->max = size;
     return q;
@@ -70,29 +73,29 @@ void print_queue(Queue *q)
     printf("\n");
 }
 
-void enqueue(Queue *q, int a)
+void enqueue(Queue *q, int a)                           // вставка в очередь
 {
-    if (q->count == q->max)
+    if (q->count == q->max)                             // проверка если очередь переполнена
     {
         fprintf(stderr, "Not enough space in queue\n");
         return;
     }
 
-    if (q->count == 0)
+    if (q->count == 0)                                  // проверка, если очередь пуста
     {
         q->data[q->tail] = a;
         q->count++;
         return;
     }
 
-    q->tail = (q->tail + 1) % q->max;
+    q->tail = (q->tail + 1) % q->max;   
     q->data[q->tail] = a;
     q->count++;
 }
 
-int dequeue(Queue *q)
+int dequeue(Queue *q)                                   // удаление элемента из очереди
 {
-    if (q->count == 0)
+    if (q->count == 0)                                  // проверка, если очередь пуста
     {
         printf("No elements in queue");
         return -1;
@@ -104,7 +107,7 @@ int dequeue(Queue *q)
     return res;
 }
 
-void print_data(Queue *q)
+void print_data(Queue *q)                               // печать всего массива data
 {
     for (int i = 0; i < q->max; i++)
         printf("%d ", q->data[i]);
